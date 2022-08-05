75effefe3264a34ef79f588011c792d5a1984c48
szsctt <suzannesctt@gmail.com>
2022-08-05 13:33:58 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,32 +12,37 @@ sudo apt-get update &&\
 cp vimrc ~/.vimrc
 
 
-if [ ! which conda ]; then
+if  ! which conda; then
 PYTHON_MINICONDA_VER='py39'
 MINICONDA_VER='4.12.0'
 
 # miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-${PYTHON_MINICONDA_VER}_${MINICONDA_VER}-Linux-x86_64.sh
 bash Miniconda3-${PYTHON_MINICONDA_VER}_${MINICONDA_VER}-Linux-x86_64.sh
+rm Miniconda3-${PYTHON_MINICONDA_VER}_${MINICONDA_VER}-Linux-x86_64.sh
 fi
 # docker
 
+if ! which docker; then
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
+rm get-docker.sh
+fi
 
 # singularity 
 
+if ! which singularity; then
 sudo apt-get install -y \
    build-essential \
    libseccomp-dev \
    pkg-config \
    squashfs-tools \
-   cryptsetup
+   cryptsetup \
+   libglib2.0-dev
 
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz &&\
-      sudo tar -C /usr/local -xzvf go1.19.linux.tar.gz	
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
-  rm go$VERSION.$OS-$ARCH.tar.gz
+      sudo tar -C /usr/local -xzvf go1.19.linux-amd64.tar.gz &&\
+      rm go1.19.linux-amd64.tar.gz
 
 echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
   source ~/.bashrc
@@ -50,3 +55,4 @@ export VERSION=3.10.0 && # adjust this as necessary \
 ./mconfig && \
     make -C builddir && \
     sudo make -C builddir install
+fi
